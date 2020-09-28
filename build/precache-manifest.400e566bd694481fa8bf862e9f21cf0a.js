c9eb4621876124f3f8bf3b2bc4965e456c35fc87
dafp18 <dafp18@hotmail.com>
2020-09-27 20:59:11 -0500

    cuarto envio

---
+++ build/precache-manifest.400e566bd694481fa8bf862e9f21cf0a.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0967df75d0129011594c5bb54bba770",
+    "url": "//kronoappwms.com/testReact/index.html"
+  },
+  {
+    "revision": "87e21e705b8faaf085d8",
+    "url": "//kronoappwms.com/testReact/static/css/main.c5a3ee5c.chunk.css"
+  },
+  {
+    "revision": "90f8357ba7ca8ef8fec4",
+    "url": "//kronoappwms.com/testReact/static/js/2.6baaa46d.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "//kronoappwms.com/testReact/static/js/2.6baaa46d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87e21e705b8faaf085d8",
+    "url": "//kronoappwms.com/testReact/static/js/main.401f0ca9.chunk.js"
+  },
+  {
+    "revision": "aea58ee06b946db1d946",
+    "url": "//kronoappwms.com/testReact/static/js/runtime-main.2c349ab9.js"
+  },
+  {
+    "revision": "b7c6144cd527af5a1e5a3222bb4674a2",
+    "url": "//kronoappwms.com/testReact/static/media/Diseno_arq.b7c6144c.svg"
+  },
+  {
+    "revision": "c3b20f065f01725f2b9848004216ecc3",
+    "url": "//kronoappwms.com/testReact/static/media/Especif_requisitos.c3b20f06.svg"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "//kronoappwms.com/testReact/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "9652d69f9885f7476b859950eacedf21",
+    "url": "//kronoappwms.com/testReact/static/media/bg4.9652d69f.svg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "//kronoappwms.com/testReact/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "7de8caaa64edf2c8fed2da75b39a5222",
+    "url": "//kronoappwms.com/testReact/static/media/biblio.7de8caaa.svg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "//kronoappwms.com/testReact/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "//kronoappwms.com/testReact/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "//kronoappwms.com/testReact/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "48402fa51ccbfd33d8b345bdf41da754",
+    "url": "//kronoappwms.com/testReact/static/media/cod_testing.48402fa5.svg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "//kronoappwms.com/testReact/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "3f6d05f823845e14a8cc3d5319a5568a",
+    "url": "//kronoappwms.com/testReact/static/media/diseno.3f6d05f8.svg"
+  },
+  {
+    "revision": "5d34befd6b5887024e654e5aa83cd157",
+    "url": "//kronoappwms.com/testReact/static/media/integra_pruebas.5d34befd.svg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "//kronoappwms.com/testReact/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "//kronoappwms.com/testReact/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "//kronoappwms.com/testReact/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "f84515b26b41ac439164403bf3dfc845",
+    "url": "//kronoappwms.com/testReact/static/media/methods.f84515b2.svg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "//kronoappwms.com/testReact/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "f3de3fe272b07d7a399291698dbba33f",
+    "url": "//kronoappwms.com/testReact/static/media/opera_mtto.f3de3fe2.svg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "//kronoappwms.com/testReact/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "//kronoappwms.com/testReact/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "//kronoappwms.com/testReact/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "//kronoappwms.com/testReact/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "//kronoappwms.com/testReact/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "//kronoappwms.com/testReact/static/media/studio-5.ef5c30ea.jpg"
+  },
+  {
+    "revision": "796367795ea2e194de4bb93916589ef6",
+    "url": "//kronoappwms.com/testReact/static/media/usabilidad.79636779.svg"
+  }
+]);
